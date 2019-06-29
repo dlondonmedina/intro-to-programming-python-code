6a8997fea384984b481f4d52f63f1d72f3d7b6a9
Dlondonmedina <medinad@uw.edu>
2019-06-28 22:44:59 -0700

    added starter code in main.py for exercises 1-2

---
+++ 1-2/main.py
@@ -0,0 +1,28 @@
+# Question 1
+name = input()
+print()
+
+# Question 2
+hours = input()
+rate = input()
+# do your calculations and prints here.
+# don't forget to convert hours and rate to
+# integers or floats accordingly.
+
+# Question 3
+fahrenheit = input()
+# your calculation goes here
+
+# Question 4
+income1 = input()
+income2 = input()
+income3 = input()
+
+totalincome = income1 + income2 + income3
+# calculate percentages for each income
+
+bill = input()
+
+# calculate the share for each of three people.
+
+# print values
